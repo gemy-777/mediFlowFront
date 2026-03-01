36e7d47be0d7cb7e988a953ecb7797ce6dec305e
Gemy <elhawaremohamed7@gmail.com>
2026-03-01 19:58:35 +0200

    myAppointment

--- src/app/pages/my-appointment/my-appointment.ts
+++ src/app/pages/my-appointment/my-appointment.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DoctorsService } from '../../services/doctors-service';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-my-appointment',
-  imports: [],
+  imports: [SlicePipe],
   templateUrl: './my-appointment.html',
   styleUrl: './my-appointment.css',
 })
 export class MyAppointment {
-
+  doctorsService = inject(DoctorsService);
+  allDoctors = this.doctorsService.doctors();
 }
